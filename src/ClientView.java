fd7ebda85bd7018c174187aa7738012c91e49c11
Justin <stuehmer@ualberta.ca>
2017-03-29 10:46:06 -0600

    Added /src directory

---
+++ src/ClientView.java
@@ -0,0 +1,14 @@
+public ClientView {
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            throw new IllegalArgumentException("ClientView expects an argument");
+        }
+        int port = Integer.parseInt(args[0]);
+
+
+        Scanner in = new Scanner(System.in);
+        
+
+}
